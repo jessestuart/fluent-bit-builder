639b2fcc74d6e6ef024dfc8227cecfb8c56ff91b
Jesse Stuart <hi@jessestuart.com>
2018-05-22 00:33:09 -0400

    [commit message skipped]

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -4,7 +4,9 @@
 # between `run:` commands without the `$BASH_ENV` dance.
 export IMAGE="jessestuart/fluent-bit-builder"
 export VERSION="$(date -u +'%Y%m%d')"
+export MANIFEST_PLATFORMS='linux/amd64,linux/arm64'
 
+echo "export MANIFEST_PLATFORMS=${MANIFEST_PLATFORMS}" >> $BASH_ENV
 echo "export IMAGE=${IMAGE}" >> $BASH_ENV
 echo "export VERSION=${VERSION}" >> $BASH_ENV
 echo 'export IMAGE_ID="${IMAGE}:${VERSION}-${TAG}"' >> $BASH_ENV
--- .circleci/push_manifest.sh
+++ .circleci/push_manifest.sh
@@ -2,8 +2,6 @@
 
 source $BASH_ENV
 
-export PLATFORMs="linux/amd64,linux/arm64"
-
 echo "Downloading manifest-tool."
 wget https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
 mv manifest-tool-linux-amd64 /usr/bin/manifest-tool
@@ -14,12 +12,12 @@ manifest-tool --version
 
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin;
 manifest-tool push from-args \
-  --platforms "$PLATFORMS" \
+  --platforms "$MANIFEST_PLATFORMS" \
   --template "$IMAGE:$VERSION-ARCH" \
   --target "$IMAGE:$VERSION"
 if [ "${CIRCLE_BRANCH}" == 'master' ]; then
   manifest-tool push from-args \
-    --platforms "$PLATFORMS" \
+    --platforms "$MANIFEST_PLATFORMS" \
     --template "$IMAGE:$VERSION-ARCH" \
     --target "$IMAGE:latest"
 fi
